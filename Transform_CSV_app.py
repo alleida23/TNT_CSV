21b858b789642cc4fde9bd4226bb0c28f2b806c6
Albert Lleida <124719215+alleida23@users.noreply.github.com>
2023-12-03 16:08:58 +0100

    Update Transform_CSV_app.py

--- Transform_CSV_app.py
+++ Transform_CSV_app.py
@@ -1,12 +1,11 @@
-# Import necessary libraries
 import streamlit as st
 import pandas as pd
 from datetime import datetime
 import subprocess
+from io import BytesIO
 
 # Install openpyxl within the Streamlit app
 subprocess.run(["pip", "install", "openpyxl"])
-
 
 # Streamlit app title
 st.title("TNT Report CSV Converter To EXCEL")
@@ -29,8 +28,10 @@ if "conversion_done" not in st.session_state:
 if st.button("Convert") or st.session_state.conversion_done:
     if uploaded_file is not None:
         try:
-            # Read the content of the uploaded CSV file
+            
+            # 0) Read the content of the uploaded CSV file
             csv = pd.read_csv(uploaded_file, sep=r',""', header=0, encoding="utf-8", engine='python')
+            
             # 1) Remove leading and trailing whitespaces from column names and all values in all columns
             csv.columns = csv.columns.str.strip('"')
             csv = csv.apply(lambda x: x.str.strip('"'))
@@ -54,26 +55,32 @@ if st.button("Convert") or st.session_state.conversion_done:
             for col in columns_to_capitalize:
                 csv[col] = csv[col].str.title()
                 
-            # Generate a timestamp
+            # 7) Generate a timestamp
             timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
                 
             # Specify the desired Excel file name
             excel_file_name = f'TNT_Track_Report_{timestamp}.xlsx'
             
-            st.write(csv.head(2))
-            st.write(f"{excel_file_name}")
+            #st.write(csv.head(2))
+            #st.write(f"{excel_file_name}")
 
-            # Save the Excel file without using openpyxl
-            csv.to_excel(excel_file_name, index=False)
+            # Create a BytesIO object to store Excel file in memory
+            excel_data = BytesIO()
+
+            # Write the Excel file to the BytesIO object
+            csv.to_excel(excel_data, index=False, engine='openpyxl')
 
             # Add a download button for the Excel file
             st.download_button(
                 label="Download Excel File",
-                data=None,
+                data=excel_data.getvalue(),
                 file_name=excel_file_name,
                 key="download_excel_button",
                 help="Click here to download the Excel file."
             )
+
+            # Set the conversion flag to True
+            st.session_state.conversion_done = True
 
         except Exception as e:
             # Handle exceptions, e.g., invalid CSV format
