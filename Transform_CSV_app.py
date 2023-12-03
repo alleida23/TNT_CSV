23dcf6985440d747abe80af7198755a1a8ab1048
Albert Lleida <124719215+alleida23@users.noreply.github.com>
2023-12-03 15:59:31 +0100

    Update Transform_CSV_app.py

--- Transform_CSV_app.py
+++ Transform_CSV_app.py
@@ -2,7 +2,6 @@
 import streamlit as st
 import pandas as pd
 from datetime import datetime
-from io import BytesIO
 
 # Streamlit app title
 st.title("TNT Report CSV Converter To EXCEL")
@@ -59,17 +58,12 @@ if st.button("Convert") or st.session_state.conversion_done:
             st.write(csv.head(2))
             st.write(f"{excel_file_name}")
 
-            # Create a BytesIO object to store Excel file in memory
-            excel_data = BytesIO()
-
-            # Write the Excel file to the BytesIO object
-            csv.to_excel(excel_data, index=False, engine='openpyxl')
+            excel_file = csv.to_excel(excel_file_name, index=False)
 
             # Add a download button for the Excel file
             st.download_button(
                 label="Download Excel File",
-                data=excel_data.getvalue(),
-                key="download_excel_button",
+                data=excel_file,
                 file_name=excel_file_name,
                 help="Click here to download the Excel file."
             )
